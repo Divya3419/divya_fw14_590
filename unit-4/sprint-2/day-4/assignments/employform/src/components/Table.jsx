d86789643ae9ab10874665b9972cd3e60345a8e8
Divya <divyajain3419@gmail.com>
2025-12-04 11:49:29 +0530

    from update

--- unit-4/sprint-2/day-4/assignments/employform/src/components/Table.jsx
+++ unit-4/sprint-2/day-4/assignments/employform/src/components/Table.jsx
@@ -1,58 +1,44 @@
-import axios from 'axios';
-import React,{useState,useEffect} from 'react'
-import styles from './Employform.module.css';
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import styles from "./Employform.module.css";
 
 const Table = () => {
-    const[show,setShow]=useState([])
+  const [show, setShow] = useState([]);
 
-    useEffect(()=>{
-        axios.get(`http://localhost:3002/employ`)
-        .then((r)=>{
-          setShow(r.data)
-        
-        })
-      // let data=async()=>{
-      // let r=await axios.get("http://localhost:8080/todos")
-      //   //console.log(r.data)
-      //   setTodo(r.data)
-      // }
-      // data();
-      },[])
+  useEffect(() => {
+    axios.get(`http://localhost:3002/employ`).then((r) => {
+      setShow(r.data);
+    });
+  }, []);
   return (
     <div>
-    <table className={styles.tab} border="1">
+      <table className={styles.tab} border="1">
         <thead>
-        <tr>
-    <th>NAME</th>
-    <th>AGE</th>
-    <th>ADDRESS</th>
-    <th>DEPARTMENT</th>
-    <th>SALARY</th>
-    <th>MARITAL STATUS</th>
-    </tr>
+          <tr>
+            <th>NAME</th>
+            <th>AGE</th>
+            <th>ADDRESS</th>
+            <th>DEPARTMENT</th>
+            <th>SALARY</th>
+            <th>MARITAL STATUS</th>
+          </tr>
         </thead>
         <tbody>
-    
-        {show.map((props)=>{
-            return(
-                <tr key={props.id}>
-<td>{props.username}</td>
-        <td>{props.age}</td>
-        <td>{props.address}</td>
-        <td>{props.department}</td>
-        <td>{props.salary}</td>
-        <td>{props.marital}</td>
-        </tr>
-            )
-        })}
-        
-    
+          {show.map((props) => {
+            return (
+              <tr key={props.id}>
+                <td>{props.username}</td>
+                <td>{props.age}</td>
+                <td>{props.address}</td>
+                <td>{props.department}</td>
+                <td>{props.salary}</td>
+                <td>{props.married}</td>
+              </tr>
+            );
+          })}
         </tbody>
-    </table>
-      </div>
-
- 
-  
-  )
-}
-export default Table
+      </table>
+    </div>
+  );
+};
+export default Table;
